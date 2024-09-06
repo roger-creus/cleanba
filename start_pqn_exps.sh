f183c9ac194fb3f7d4b70095eca33b63243c9bfe
roger <creus99@gmail.com>
2024-09-05 21:05:34 -0400

    changed launchers to long partition:

--- start_pqn_exps.sh
+++ start_pqn_exps.sh
@@ -1,6 +1,20 @@
+# breakout
 sbatch train_real cleanba/cleanba_pqn.py --track
-
 sbatch train_real cleanba/cleanba_impala.py --track
-
 sbatch train_real cleanba/cleanba_ppo.py --track
+
+# pong
+sbatch train_real cleanba/cleanba_pqn.py --env_id=Pong-v5 --track
+sbatch train_real cleanba/cleanba_impala.py --env_id=Pong-v5 --track
+sbatch train_real cleanba/cleanba_ppo.py --env_id=Pong-v5 --track
+
+# pacman
+sbatch train_real cleanba/cleanba_pqn.py --env_id=MsPacman-v5 --track
+sbatch train_real cleanba/cleanba_impala.py --env_id=MsPacman-v5 --track
+sbatch train_real cleanba/cleanba_ppo.py --env_id=MsPacman-v5 --track
+
+# SpaceInvaders
+sbatch train_real cleanba/cleanba_pqn.py --env_id=SpaceInvaders-v5 --track
+sbatch train_real cleanba/cleanba_impala.py --env_id=SpaceInvaders-v5 --track
+sbatch train_real cleanba/cleanba_ppo.py --env_id=SpaceInvaders-v5 --track
 
